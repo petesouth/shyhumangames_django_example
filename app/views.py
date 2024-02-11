7c77f80968885295b02f976041f56003d5b2427c
Peter Southwell <pete_south@yahoo.com>
2024-02-11 23:59:38 +0000

    filter by city search by name

--- app/views.py
+++ app/views.py
@@ -1,20 +1,36 @@
-from rest_framework import generics, response
-from rest_framework.pagination import CursorPagination
+from django.db.models import Q
+from rest_framework import generics
 from .models import Supplier
 from .serializers import SupplierSerializer
 from django.http import JsonResponse
+from rest_framework.pagination import CursorPagination
 
 class SupplierPagination(CursorPagination):
     ordering = '-popularity'
-    page_size = 10  # Adjust as needed
+    page_size = 20  # Adjust as needed
     cursor_query_param = 'cursor'
     cursor_page_query_param = 'cursor'
-
 
 class SupplierListView(generics.ListAPIView):
     serializer_class = SupplierSerializer
     pagination_class = SupplierPagination
-    queryset = Supplier.objects.all()  # Add this line
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned suppliers to a given city,
+        by filtering against a `city` query parameter in the URL.
+        """
+        queryset = Supplier.objects.all()
+        city = self.request.query_params.get('city', None)
+        search = self.request.query_params.get('search', None)
+
+        if city is not None:
+            queryset = queryset.filter(city=city)
+
+        if search is not None:
+            queryset = queryset.filter(name__icontains=search)
+
+        return queryset
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
