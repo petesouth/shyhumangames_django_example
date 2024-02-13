6c661fcf9d8d7dd9de543e5f3f22f8f2eaf756c7
Peter Southwell <pete_south@yahoo.com>
2024-02-13 22:52:12 +0000

    Url fix

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('v2/values', views.KeyValueJsonStoreAPIView.as_view(), name='key-value-store'),
+    path('v2/values/', views.KeyValueJsonStoreAPIView.as_view(), name='key-value-store'),
     path('v2/suppliers/', views.SupplierListView.as_view(), name='supplier-list'),
 ]
 
